e39b0789b77b6dd09c395fc86ebb46fce8b02927
Chidubem Uba <chidubem.uba.250368@unn.edu.ng>
2022-06-29 18:22:38 -0700

    ongoing work on firebase login

--- app/src/main/java/com/example/pixcy/LoginActivity.java
+++ app/src/main/java/com/example/pixcy/LoginActivity.java
@@ -3,11 +3,15 @@ package com.example.pixcy;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+
 import com.example.pixcy.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding activityLoginBinding;
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,8 @@ public class LoginActivity extends AppCompatActivity {
         activityLoginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = activityLoginBinding.getRoot();
         setContentView(view);
+
+
 
     }
 }
--- app/src/main/java/com/example/pixcy/MainActivity.java
+++ app/src/main/java/com/example/pixcy/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.pixcy;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.example.pixcy.fragments.ComposeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +31,21 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.action_compose:
                         Toast.makeText(MainActivity.this, "Compose", Toast.LENGTH_SHORT).show();
-                        //fragment = fragment1;
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_memories:
+                        // TODO: update fragment
                         Toast.makeText(MainActivity.this, "Memories", Toast.LENGTH_SHORT).show();
-                        //fragment = fragment2;
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_map:
                     default:
+                        // TODO: update fragment
                         Toast.makeText(MainActivity.this, "Map", Toast.LENGTH_SHORT).show();
-                        //fragment = fragment3;
+                        fragment = new ComposeFragment();
                         break;
                 }
-                //fragmentManager.beginTransaction().replace(R.id.rlContainer, fragment).commit();
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
